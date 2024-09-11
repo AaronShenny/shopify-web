25b667591b68d95a186659b37a30801eaea62593
AaronShenny <aaronshenny102.go@gmail.com>
2024-09-11 20:31:19 +0530

    hello

--- shop/models.py
+++ shop/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class CreateUser(models.Model):
+
+class Customers(models.Model):
     name = models.CharField(max_length=100)
     password = models.TextField(max_length=1000)
-    emailid = models.CharField(max_length=1000)
+    emailid = models.CharField(max_length=1001)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+class Product(models.Model):
+    pname = models.CharField(max_length=100)
+    pdesc = models.TextField(max_length=1000)
+    stock = models.IntegerField(max_length=1000)
+    pprice = models.IntegerField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    images = models.ImageField(upload_to='images/',null=True , blank=True)
+    def __str__(self):
+        return self.pname
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Product
+from .form import ProductForm
 # Create your views here.
 def shop(request):
-    navbar = {'show_user_navbar': True}  
-    return render(request, "index.html",navbar)
+    products = Product.objects.all()  
+    navbar = {'show_user_navbar': True}
+    if request.POST:
+        frm = ProductForm(request.POST,request.FILES)
+        print(request.FILES) 
+        if frm.is_valid():
+            frm.save()
+            return redirect('create')
+    else:
+        frm = ProductForm()
+    print(products)  # Add this line to check if products are being fetched
+    return render(request, "index.html", {'products': products, 'navbar': navbar})
+
+
+def CreateProduct(request):
+    navbar = {'show_user_navbar': True}
+    if request.POST:
+        frm = ProductForm(request.POST,request.FILES)
+        print(request.FILES) 
+        if frm.is_valid():
+            frm.save()
+            return redirect('/')
+    else:
+        frm = ProductForm()
+     # Add this line to check if products are being fetched
+    return render(request, "create.html", {'navbar': navbar,'frm':frm})
+
+def addtocart(request):
+    if request.method ==  'POST':
+        print(request.POST)
